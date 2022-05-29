652d071773d54417442ed1a01d97914ce4a133cb
DaliaZegaya <daliazegaya56@gmail.com>
2022-05-29 22:58:28 +0300

    commit

---
+++ main.js
@@ -0,0 +1,79 @@
+
+// console.log (first_id.innerText) 
+
+// second_id.innerText= "My DOM App"
+
+// var firstClass= document.getElementsByClassName("first_class")
+// console.log(firstClass);
+
+// console.log(firstClass[1]);
+
+// console.log(firstClass[2]);
+
+// var secondClass= document.getElementsByClassName("second_class")
+// console.log(secondClass);
+
+// function printName () {
+//     var userName= prompt ("your name")
+//     document.write ("<h2>"+userName+"<h2>")
+
+// }
+// printName ()
+
+// function printColor () {
+//     var userColor= prompt ("enter your color")
+//     var userNumber= +prompt ("enter your number")
+//     for (var i=0; i<userNumber; i++) {
+//         document.write ("<span>"+userColor+"<br>"+"<span>")
+//     }
+// }
+// printColor ()
+
+// function tagFunction1 () {
+//     var userText= prompt ("enter your text")
+//     var userTag= prompt ("enter your tag name")
+//     document.write ("<"+userTag+">"+userText+"<"+userTag+"/>")
+// }
+// tagFunction()
+
+// function tagFunction2 () {
+//     var tagg= prompt ("enter your tag")
+//     var textt= prompt ("enter your text")
+//     var result= document.getElementById(tagg)
+//     result.innerText= textt
+// }
+// tagFunction2 ()
+
+// function someFunction () {
+//     var userInput= prompt ("enter your input")
+//     var userText= prompt ("enter your text")
+//     document.write ("<"+userInput+">"+userText+"<"+userInput+"</>")
+// }
+// someFunction ()
+
+// function classFunction (className, textt) {
+//     className= document.getElementsByClassName("my class") = textt
+//     document.write (className)
+// }
+// classFunction (prompt("class name"), prompt ("your text"))
+
+// function spanFunction () {
+//     var pClass= document.getElementsByTagName ("p")
+//     for (var i=0; i<6;i++) {
+//         pClass[i].innerHTML= "<span>"+"hello world"+"</span"
+//     }
+//     document.write (pClass)
+// }
+// spanFunction ()
+
+// function printInfo () {
+//     var userName= prompt ("enter your name") 
+//     var lastName= prompt ("enter your last name")
+//     var age= +prompt ("enter your age") 
+//     document.write (userName+lastName+age)
+//     if (age>18) {
+//         document.write (userName+lastName+age+"<br>"+"<input>")
+//     }
+// }
+// printInfo ()
+
